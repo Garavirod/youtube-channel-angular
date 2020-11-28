2c2223dda0ccfb242bfdee82e1513e2536349182
Rodrigo García Ávila <rodrigogarciaavila26@gmail.com>
2020-11-27 20:23:29 -0600

    Youtube models was created based on QuickType json to typescript

--- src/app/services/youtube.service.ts
+++ src/app/services/youtube.service.ts
@@ -1,5 +1,7 @@
+import { YoutubeResponse } from './../models/youtube.models';
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -20,6 +22,13 @@ export class YoutubeService {
     .set('maxResults',`${this.maxResults}`)
     .set('playlistId',this.playListid)
     .set('key',this.apiKey);
-    return this.http.get(endpoint, {params:params});
+    return this.http.get<YoutubeResponse>(endpoint, {params:params})
+            .pipe(
+              map((resp)=>{
+                this.nextPageToken= resp.nextPageToken;
+                return resp.items;
+              }),
+              map(items=>items.map( video => video.snippet ))
+            )
   }
 }
